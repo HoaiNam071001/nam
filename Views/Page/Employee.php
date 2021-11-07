d7fdc93d3a0a6123f8b994dfe3731fb18aa3af05
HoaiNam071001 <nam215543694@gmail.com>
2021-11-07 14:48:43 +0700

    main

--- Views/Page/Employee.php
+++ Views/Page/Employee.php
@@ -16,7 +16,6 @@
                         <span>Customer</span>
                         <span><?= $val['NAMECUST'];?></span>
                     </div>
-                    <button class="btn" style="width:80px;height:50px;">Xóa</button>
 
                 </div>
                 <a href="index.php?controller=Payment&id=<?= $val['IDCART'];?>" class="btn">PAYMENT</a>
